7961fbe6ae40fde4b8bf284ec5de1a6e21d7cba3
伐薪 <huiqi.xhq@alibaba-inc.com>
2014-09-20 13:41:28 +0800

    drag

--- src/ActiveFlow.js
+++ src/ActiveFlow.js
@@ -73,18 +73,37 @@ define(['jquery','FlowParser','FlowPlayer','PlayNode','Snap'], function ($ , Flo
 
     ActiveFlow.prototype.draggable = function () {
         var s = Snap("svg>g");
+
         var that = s;
 
         function onMove(dx, dy, x, y, evt) {
             that.attr({
-                transform: "translate(" + dx + ", " + dy + ")"
+                transform:"translate("+(dx)+", "+(dy)+")"
             });
         }
 
+        function onStart(x, y, evt) {
+                that.originalX = x;
+                that.originalY = y;
+        }
 
-        s.drag(onMove);
+        s.drag(onMove, onStart);
     };
 
     return ActiveFlow;
 });
 
+function getTranslate(str){
+    if(!str){
+        return {x:0,y:0};
+    }
+    var p = str.indexOf("(");
+    str = str.substr(p + 1);
+    var arr = str.split(",");
+    console.log(arr);
+
+    return {
+        x:parseFloat(arr[4]),
+        y:parseFloat(arr[5])
+    }
+}
