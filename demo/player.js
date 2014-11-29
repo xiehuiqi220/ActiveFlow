8bbb4d9cd4665c3ea14b5c3da80878b59bfaa955
伐薪 <huiqi.xhq@alibaba-inc.com>
2014-11-29 23:08:18 +0800

    file

--- demo/player.js
+++ demo/player.js
@@ -13,30 +13,43 @@ requirejs.config({
 
 require(['jquery','Snap','ActiveFlow'], function ($ , Snap , ActiveFlow) {
 
-    $(function(){
+    function upload(content) {
+        $.post("http://node.ewikisoft.com:3000/file/upload?_=" + new Date().getTime(), { fileContent: content}, function (data) {
+            alert("Data Loaded: " + data);
+        });
+    }
+
+    function getFile(id){
+        $.get("http://node.ewikisoft.com:3000/file/get?_=" + new Date().getTime(), { id: id}, function (data) {
+            alert("Data Loaded: " + data);
+        });
+    }
+
+    $(function () {
         var
-        af
-        inputFile = $('#file'),
-        isAuto = false,
-        useTTS = true,
-        container = $("#divCanvas");
-        $(window).on('resize',function(){
+            af,
+            inputFile = $('#file'),
+            isAuto = false,
+            useTTS = true,
+            container = $("#divCanvas");
+        $(window).on('resize', function () {
             container.height($(window).height());
         }).trigger('resize');
 
-        container.on('click',function(){
+        container.on('click', function () {
             inputFile.trigger('click');
         });
-
-        inputFile.on('change', function(){
+        getFile(12);
+        inputFile.on('change', function () {
             var file = this.files[0]; // FileList object
             var reader = new FileReader();
             reader.readAsText(file);
-            reader.onload = function(){
+            reader.onload = function () {
                 $(window).off('resize');
                 container.off('click');
                 container.height('auto');
                 var ret = this.result;
+                upload(ret);
                 container.html(ret);
                 af = new ActiveFlow(container.find('svg')[0]);
                 af.init();
@@ -44,47 +57,47 @@ require(['jquery','Snap','ActiveFlow'], function ($ , Snap , ActiveFlow) {
             };
         });
 
-        var controllerInit = function(){
+        var controllerInit = function () {
             var toolbar = $('#toolbar');
-            toolbar.find('.prev').on('click',function(){
+            toolbar.find('.prev').on('click', function () {
                 af.prev();
             });
-            toolbar.find('.next').on('click',function(){
+            toolbar.find('.next').on('click', function () {
                 af.next();
             });
-            toolbar.find('.play').on('click',function(){
+            toolbar.find('.play').on('click', function () {
                 af.start();
             });
-            toolbar.find('.auto .button').on('click',function(){
+            toolbar.find('.auto .button').on('click', function () {
                 isAuto = !isAuto;
                 af.auto(isAuto);
-                if(isAuto){
+                if (isAuto) {
                     $(this).removeClass('off').addClass('on');
                 }
-                else{
+                else {
                     $(this).removeClass('on').addClass('off');
                 }
             }).removeClass('on').addClass('off');
-            toolbar.find('.voice .button').on('click',function(){
+            toolbar.find('.voice .button').on('click', function () {
                 useTTS = !useTTS;
                 af.TTS(useTTS);
-                if(useTTS){
+                if (useTTS) {
                     $(this).removeClass('off').addClass('on');
                 }
-                else{
+                else {
                     $(this).removeClass('on').addClass('off');
                 }
             }).removeClass('off').addClass('on');
 
-            toolbar.find('.type').on('click',function(){
-                var license = prompt('Enter license','show me the money');
-                if(license){
+            toolbar.find('.type').on('click', function () {
+                var license = prompt('Enter license', 'show me the money');
+                if (license) {
                     alert('Your license is invalid');
                     af.egg();
                 }
             });
 
-            toolbar.find('.speed input').on('change',function(){
+            toolbar.find('.speed input').on('change', function () {
                 af.setSpeed($(this).val());
             }).trigger('change');
         }
